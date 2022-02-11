b28b49af28ecc6d8a603cc08025bcef9b99e72c0
ChescaRobertson <francescamarierobertson@hotmail.co.uk@>
2022-02-11 15:25:35 +0000

    Refactor

--- Chapter 10/script.js
+++ Chapter 10/script.js
@@ -5,11 +5,11 @@ let paragraph = document.querySelector('#para');
 showButton.addEventListener('click', function () {
   paragraph.style.display = 'block';
   hideButton.style.display = 'block';
-  showButton.style.display = 'none';
+  this.style.display = 'none';
 });
 
 hideButton.addEventListener('click', function () {
   paragraph.style.display = 'none';
-  hideButton.style.display = 'none';
+  this.style.display = 'none';
   showButton.style.display = 'block';
 });
